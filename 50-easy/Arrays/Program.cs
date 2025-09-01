75aedb4e4d93eb2e02f7f346170894aa4a4cce41
Roeiazran <roei@h-MacBook-Air-sl-rwy.local>
2025-09-01 23:28:17 +0300

    updated 50-easy Arrays

--- 50-easy/Arrays/Program.cs
+++ 50-easy/Arrays/Program.cs
@@ -5,38 +5,46 @@
     }
 
     /*
-        Problem: Given an array nums sorted in non-decreasing order, bring to front al the unique values in nums to appear only once
-        Idea: Increment right until nums[right] != nums[right + 1] and store nums[right] into nums[left]
-        Complexity: O(n)
+        Problem desc: Given a sorted array nums, remove the duplicates in-place such that
+        each element appears only once and return the new length.
+        Do not allocate extra space; modify the array in-place with O(1) extra memory.
+
+        Idea: Loop through nums array and skip duplicates by checking nums[r] and nums[r - 1],
+        only insert into nums[l] if nums[r] != nums[r - 1].
+
+        Time Complexity: O(n) - We iterate through the array with one time with r pointer.
+        Space Complexity: O(1).
     */
     public static int RemoveDuplicates(int[] nums)
     {
+        int n = nums.Length;
+        if (n == 0) return 0;
 
-        int left = 0; int n = nums.Length;
+        int l = 1;
 
-        for (int right = 0; right < n; right++)
+        for (int r = 1; r < n; r++)
         {
-            while (right < n - 1 && nums[right] == nums[right + 1]) right++;
-            nums[left] = nums[right];
-            left++;
+            if (nums[r] != nums[r - 1]) {
+                nums[l] = nums[r];
+                l++;
+            }
         }
-
-        return left;
+        return l;
     }
 
     /*
-        Problem: You have an array prices[] where prices[i] is the stock price on day i.
-        You can buy and sell multiple times, but you cannot hold more than one stock at a time.
-        Goal: maximize total profit.
-        Insight: Any sequence where the price goes up from one day to the next is an opportunity to earn.
-        You don’t need to pick the absolute minima or maxima.
-        Idea: Loop through the prices from day 1 to day N - 1 and compare today's price with yesterday, if it's a profit add it 
-        to the total profit otherwise do nothing. “Every time the price goes up, capture that increase.”
-        Complexity: O(n).
+        Problem desc: You are given an integer array prices where prices[i] is the price of a stock on the ith day.
+        You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times),
+        but you must sell before you buy again. Return the maximum profit you can achieve.
+
+        Idea: Instead of trying to find valleys and peaks we sum up all the positive 
+        differences between consecutive days.
+
+        Time Complexity: O(n) - One loop on the prices array.
+        Space Complexity: O(1).
     */
     public static int MaxProfit(int[] prices)
     {
-
         int n = prices.Length, maxProfit = 0;
 
         for (int i = 0; i < n - 1; i++)
@@ -47,24 +55,36 @@
     }
 
     /*
-        Problem: Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-        Idea: Reverse the array then reverse the [1,..,k] and [k + 1,..,n] individually.
-        Complexity: O(n)
+        Problem: Given an array, rotate the array to the right by k steps, where k is non-negative.
+        You must do it in-place with O(1) extra space.
+
+        Idea: Reverse the array then reverse the subarray [1,..,k] and the subarray [k + 1,..,n] individually.
+
+        Time Complexity: O(n)
+        Space complexity: O(1)
     */
     public static void Rotate(int[] nums, int k)
     {
         int n = nums.Length;
 
-        while (k > n) k -= n;
+        k %= n;
 
         Array.Reverse(nums);
         Array.Reverse(nums, 0, k);
         Array.Reverse(nums, k, n - k);
     }
 
+
+    /*
+        Problem desc: Given an integer array nums, return true if any value appears at least twice in the array,
+        and return false if every element is distinct.
+
+        Idea: Use HashSet to keep track of numbers we've already seen.
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+    */
     public static bool ContainsDuplicate(int[] nums)
     {
-
         HashSet<int> dict = [];
         int n = nums.Length;
 
@@ -81,10 +101,12 @@
     /*
         Problem: Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
         You must implement a solution with a linear runtime complexity and use only constant extra space.
+
         Idea: Xor operation properties are: commutativity, associativity, a^a = 0, a^0 = a.
-        Complexity O(n)
+        Time Complexity O(n)
+        Space Complexity: O(1)
     */
-    public static int SingalNumber(int[] nums)
+    public static int SingleNumber(int[] nums)
     {
         int n = nums.Length, res = 0;
 
@@ -95,27 +117,36 @@
     /*
         Problem: Given two integer arrays nums1 and nums2, return an array of their intersection.
         Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+        Idea: Count frequencies using a Dictionary, and compare that frequencies with the other array.
+
+        Time Complexity: O(n + m)
+        Space Complexity: O(min(n,m))
     */
     public static int[] Intersect(int[] nums1, int[] nums2) {
 
-        int p1 = 0, p2 = 0, n1 = nums1.Length, n2 = nums2.Length;
-        List<int> res = [];
-        Array.Sort(nums1);
-        Array.Sort(nums2);
+        // We want to create a dictionary with the smallest size
+        if (nums1.Length > nums2.Length) Intersect(nums2, nums1);
 
-        while (p1 < n1 && p2 < n2)
-        {
-            if (nums1[p1] < nums2[p2]) p1++;
-            else if (nums1[p1] > nums2[p2]) p2++;
-            else
-            {
-                res.Add(nums1[p1]);
-                p1++;
-                p2++;
+        Dictionary<int, int> freq = [];
+        List<int> res = [];
+
+        // Prepare the dictionary from nums1
+        foreach (int num in nums1) {
+            if (!freq.ContainsKey(num)) freq[num] = 0;
+            freq[num]++;
+        }
+
+        foreach (int num in nums2) {
+
+            // Check if the number appears in num1 with frequency larger then 0
+            if (freq.ContainsKey(num) && freq[num] > 0) {
+                res.Add(num);
+                freq[num]--;
             }
         }
 
-        return [.. res];
+        return [.. res]; // Return the List<int> result as an int[].
     }
 
     /*
