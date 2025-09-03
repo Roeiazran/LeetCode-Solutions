f97b85a336da55eada70e40a522217c3a4d4ab47
Roeiazran <roei@h-MacBook-Air-sl-rwy.local>
2025-09-03 12:14:35 +0300

    updated 50-easy sotring

--- 50-easy/Sorting-and-searching/main.cpp
+++ 50-easy/Sorting-and-searching/main.cpp
@@ -2,50 +2,45 @@
 #include <vector>
 using namespace std;
 
+// Given util function for firstBadVersion
 bool isBadVersion(int version);
 
 /*
     Problem: Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
-    Idea: Use binary search to narrowing down the search by half each time.
+    Idea: We use binary search to narrowing down the search by half each time.
     Complexity: O(log(n)).
 */
 int firstBadVersion(int n) {
-
-    int left = 1, right = n;
-    long middle;
+    int left = 1, right = n, middle;
 
     while (left < right) {
         /* 
-        Note: possible overflow when middle = (left + right) / 2.
-        Because right and left are ints (left + right) could potentially overflow.
-        The equivalent expression left + (right - left) / 2 is Safe!
+        Warning: possible overflow when assigning middle = (left + right) / 2.
+        Right and left are ints therefor (left + right) could potentially overflow.
+        Use the equivalent expression left + (right - left) / 2 is Safe!
         */
         middle = left + (right - left) / 2;
 
-        if (isBadVersion(middle)) 
-        {
-            right = middle; // Shrink the search
-        }
-        else 
-        {
-            left = middle + 1; // Skip the good version
-        }
+        if (isBadVersion(middle)) right = middle;   // Cut the search by half
+        else left = middle + 1;                     // Skip the good version
+    
     }
     return left; // left == right == first bad version
 }
 
-
 /*
-    Problem: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively
+    Problem: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+    representing the number of elements in nums1 and nums2 respectively.
     Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-    Idea: Merge the arrays by compering elements from the end.
+
+    Idea: We walk through both arrays from the end usign two pointers.
     Complexity: O(n + m)
 */
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     
-    int m1 = m - 1; // index to the last (non-zero) item of nums1
-    int n2 = n - 1; // index to the last item of nums2
-    int nm = n + m - 1; // index used to insert items into nums1
+    int m1 = m - 1; // Pointer to the last item of nums1
+    int n2 = n - 1; // Pointer to the last item of nums2
+    int nm = n + m - 1; // Pointer to last insert position in nums1
 
     while (n2 >= 0 && m1 >= 0)
     {
