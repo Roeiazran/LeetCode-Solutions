95560f29164eb1a381c7a5c31b114acf2d04076b
Roeiazran <roei@h-MacBook-Air-sl-rwy.local>
2025-09-04 12:18:24 +0300

    improvment to 50-easy Strings

--- 50-easy/Strings/main.cpp
+++ 50-easy/Strings/main.cpp
@@ -21,27 +21,38 @@ int main(int argc, char const *argv[])
 
 /*
     Problem: Write a function to find the longest common prefix string amongst an array of strings.
-    Complexity: O(n) Where n is the number of all characters in the input
+
+    Idea: Instead of scanning each string row by row, we scan each string column by column.
+    In each column indexed i we check str[i] for each str in strs, 
+    if one has a mismatch character we stop the search.
+
+    Time Complexity: O(n) Where n is the number of all characters in the input.
+    Space Complexity: O(m) Where m is the the number of characters in the longest string in strs.
 */
 string longestCommonPrefix(vector<string>& strs) {
 
     int n = strs.size();
-    size_t minLength;
-    string prefix = "";
+    size_t minLength;   // Store the minimum length of any string in strs
+    string prefix = ""; // Store the longest common string
+    char ch;            // Store the character at each index
 
-    char ch; // temporary variable used to stores the character at index i
+    // Edge case: empty input
     if (n == 0) return "";
+
+
+    // Find the minimum length of any string in strs
     minLength = strs.at(0).size();
-    // find the minimum length of any string in strs
     for (auto& str: strs) minLength = min(str.size(), minLength);
 
+    // Find common prefix string among all the strings in strs
     for (int i = 0; i < minLength; i++)
     {
-         ch = strs[0][i];
-        // check for common prefix on all string in strs
+        ch = strs[0][i]; // Character to compare in this column
+        
+        // Try finding this character in each string in strs
         for (int j = 1; j < n; j++) if (strs[j][i] != ch) return prefix;
 
-        // append the common character
+        // All string share ch at index i, append it to the result
         prefix.push_back(ch);
     }
 
@@ -51,30 +62,33 @@ string longestCommonPrefix(vector<string>& strs) {
 
 /*
     Problem: Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-    Idea: Check each possibility like sliding window.
-    Complexity: O(n*m)
+    
+    Idea: Walk through haystack and search the substring needle in each index.
+
+    Time Complexity: O(nm)
+    Space Complexity: O(1)
 */
 int strStr(string haystack, string needle) {
-    int np = 0, hp = 0, hsize = haystack.size(), nsize = needle.size();
+    int np = 0, hp = 0; // Indices into haystack and needle
+    int hsize = haystack.size(), nsize = needle.size();
 
     while (hp < hsize)
     {
-        // advance both pointers when there content match
+        // Advance both pointers as long as the characters match
         while (hp < hsize && np < nsize && haystack[hp] == needle[np])
         {
             hp++;
             np++;
         }
 
-        // check if needle pointer is located at the end of the string
+        // If needle pointer is located at the end, match the whole needle
         if (np == nsize) return hp - nsize;
-
-        // initialize values for the next iteration
-        hp = hp - np + 1;
-        np = 0;
+        
+        hp = hp - np + 1;   // Reset haystack pointer to the next character
+        np = 0;             // Reset needle pointer to the start
     }
 
-    // default case of not found
+    // No match found
     return -1;
 }
 
@@ -83,59 +97,33 @@ int strStr(string haystack, string needle) {
     1. Whitespace: Ignore any leading whitespace (" ").
     2. Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
     3. Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
-    4. Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+    4. Rounding: If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
     Cases: "42" -> 42, " -042" -> -42, "1337c0d3" -> 1337, "0-1" -> 0 (default to 0 since no digit after 0).
 */
 int myAtoi(string s) {
-    bool is_positive = true, seen_zero = false;
-    long res = 0;
+    bool isPositive = true;
+    int res = 0;
     size_t i = 0, n = s.length();
 
-    if (n == 0) return 0;
-    
-    // skip all spaces
+    // Skip all white spaces
     while (i < n && s[i] == ' ') i++;
 
-    //skip all zeros and note if you saw zero
-    while (i < n && s[i] == '0') 
-    {
+    // Check the sign
+    if (i < n && (s[i] == '-' || s[i] == '+')){
+        if (s[i] == '-') isPositive = false;
         i++;
-        seen_zero = true;
-    } 
-
-    // in case of all zeros return 0
-    if (i == n) return 0;
-
-    // check the sign (if exists)
-    if (s[i] == '-') 
-    {
-        if (seen_zero) return 0; // if saw zero next must be digit
-        i++;
-        is_positive = false;
-        s = s.substr(i); // skip the sign
-    } else if (s[i] == '+') 
-    {
-        if (seen_zero) return 0; // same
-        i++;
-        s = s.substr(i);
-    } else 
-    {
-        s = s.substr(i);
     }
     
-    i = 0, n = s.size();
-    for (; i < n; i++)
-    {
-        // in case of a non-digit stop and return the number constructed
-        if (s[i] > '9' || s[i] < '0') return is_positive? res : - res;
-        
-        // construct the number from the string
-        res *= 10;
-        res = res + s[i] - '0';
+    while (i < n && isdigit(s[i])) {
+        int digit = s[i] - '0';
 
-        // there is must be overflowing number
-        if (res > INT_MAX) return is_positive? INT_MAX : INT_MIN;        
+        // Prevent overflow before multiplying
+        if (res > (INT_MAX - digit) / 10) return isPositive? INT_MAX : INT_MIN;
+
+        // Construct the number
+        res = res * 10 + (s[i] - '0');
+        i++;
     }
-    return is_positive? res : - res;
+    return isPositive? res : -res;
 }
 
